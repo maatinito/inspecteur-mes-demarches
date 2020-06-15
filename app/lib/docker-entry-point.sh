6e4f405d4e7f0876275bcd56b851ec10cc5d5064
maatinito <15379878+maatinito@users.noreply.github.com>
2020-06-15 11:45:56 -1000

    docker-entry-point.sh: added tracces around database migration

--- app/lib/docker-entry-point.sh
+++ app/lib/docker-entry-point.sh
@@ -6,7 +6,10 @@ if [ -f /app/tmp/pids/server.pid ]; then
   rm /app/tmp/pids/server.pid
 fi
 
+echo Migrating or creating database
 bundle exec rake db:migrate || bundle exec rake db:setup
+echo Database migration/creation done
+
 # bundle exec rake after_party:run || true
 # bundle exec rake jobs:schedule
 
