b2aa8a5e8b2e5b56c0dc1d14fd0eaa2311b490a9
maatinito <15379878+maatinito@users.noreply.github.com>
2020-07-26 09:24:49 -1000

    RSpec: Added InspectorTask, FieldChecker

--- spec/models/check_spec.rb
+++ spec/models/check_spec.rb
@@ -3,13 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe Check, type: :model do
-  let(:check) { Check.new }
+  let(:check) { create :check_with_messages }
   let(:msg) { Message.new(field: 'dossier', value: '33', message: 'message') }
 
   describe '#add_message' do
     it 'should store messages' do
       check.messages << msg
-      expect(check.messages.size).to be_equal(1)
+      expect(check.messages.size).to be_equal(2)
+      expect(check.save).to be_truthy
     end
   end
 end
