f858b6e1654bfcf32ceb88a1db885b9fa2cdc10f
maatinito <15379878+maatinito@users.noreply.github.com>
2021-04-25 18:33:31 -1000

    Resolve Zeitwerk constant namings

--- app/lib/deseti/message_deseti.rb
+++ app/lib/deseti/message_deseti.rb
@@ -1,19 +1,21 @@
 # frozen_string_literal: true
 
-class MessageDeseti < FieldChecker
-  def initialize(params)
-    super
-    @jours_ecoules = @params[:jours_ecoules]
-    @message = [*@params[:corps]].join
-  end
-
-  def check(dossier)
-    if dossier.state == 'accepte' && dossier.datePassageEnConstruction + @jours_ecoules.days > Time.zone.now
-      add_message('dossier', dossier.number, @message)
+module Deseti
+  class MessageDeseti < FieldChecker
+    def initialize(params)
+      super
+      @jours_ecoules = @params[:jours_ecoules]
+      @message = [*@params[:corps]].join
     end
-  end
 
-  def required_fields
-    super + %i[corps jours_ecoules]
+    def check(dossier)
+      if dossier.state == 'accepte' && dossier.datePassageEnConstruction + @jours_ecoules.days > Time.zone.now
+        add_message('dossier', dossier.number, @message)
+      end
+    end
+
+    def required_fields
+      super + %i[corps jours_ecoules]
+    end
   end
 end
