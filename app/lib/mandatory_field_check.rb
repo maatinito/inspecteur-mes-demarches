9b9cd6064ba49aa10cabd3d106f6b1cc6727dac9
maatinito <15379878+maatinito@users.noreply.github.com>
2021-04-25 16:02:56 -1000

    Bump ruby/rails to resolve strange behavior

---
+++ app/lib/mandatory_field_check.rb
@@ -0,0 +1,2 @@
+class MandatoryFieldCheck
+end
--- config/spring.rb
+++ config/spring.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
+Spring.after_fork do
+  if ENV['DEBUGGER_STORED_RUBYLIB']
+    ENV['DEBUGGER_STORED_RUBYLIB'].split(File::PATH_SEPARATOR).each do |path|
+      next unless /ruby-debug-ide/.match?(path)
+      load path + '/ruby-debug-ide/multiprocess/starter.rb'
+    end
+  end
+end
 
 Spring.watch(
   '.ruby-version',
