49ebd439fb6e6b0ced30b0edec653093f34543bc
maatinito <15379878+maatinito@users.noreply.github.com>
2021-06-08 15:24:02 -1000

    rubocop

--- db/migrate/20210608211528_change_data_type_for_version.rb
+++ db/migrate/20210608211528_change_data_type_for_version.rb
@@ -1,9 +1,12 @@
+# frozen_string_literal: true
+
 class ChangeDataTypeForVersion < ActiveRecord::Migration[6.1]
   def self.up
     change_table :checks do |t|
       t.change :version, :bigint
     end
   end
+
   def self.down
     change_table :checks do |t|
       t.change :version, :float
