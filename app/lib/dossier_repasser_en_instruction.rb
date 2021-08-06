1c51158270b72cf0d25ebed1d08bdb6b7707fca7
maatinito <15379878+maatinito@users.noreply.github.com>
2021-08-05 14:47:31 -1000

    Added RepasserEnInstruction mutation

---
+++ app/lib/dossier_repasser_en_instruction.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class DossierRepasserEnInstruction < DossierChangerEtat
+  Queries = MesDemarches::Client.parse <<-'GRAPHQL'
+    mutation EnInstruction($dossierId: ID!, $instructeurId: ID!, $clientMutationId: String) {
+      dossierRepasserEnInstruction(input: {
+        dossierId: $dossierId,
+        instructeurId: $instructeurId,
+        clientMutationId: $clientMutationId,
+      }) {
+        clientMutationId
+        dossier {
+          id
+          number
+          state
+        }
+        errors
+      }
+    }
+  GRAPHQL
+
+  def change_state(demarche, dossier)
+    result = MesDemarches::Client.query(Queries::EnInstruction, variables:
+      {
+        dossierId: dossier.id,
+        instructeurId: demarche.instructeur
+      })
+    throw StandardError.new result.errors if result.errors.present?
+  end
+end
