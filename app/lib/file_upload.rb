35e3d74983bc807d3d4121f3b0428c800ac1d158
maatinito <15379878+maatinito@users.noreply.github.com>
2022-03-01 07:47:18 -1000

    Set Annotation Value refactoring: file upload put in separate class

---
+++ app/lib/file_upload.rb
@@ -0,0 +1,50 @@
+class FileUpload
+  def self.upload_file(dossier_id, path, filename, checksum = checksum(path))
+    slot = upload_slot(dossier_id, checksum, path, filename)
+    params = slot.direct_upload
+    response = Typhoeus.put(params.url, headers: JSON.parse(params.headers), body: File.read(path, mode: 'rb'))
+    throw response.response_body if response.code != 200
+    params.signed_blob_id
+  end
+
+  def self.upload_slot(dossier_id, checksum, path, filename)
+    result = MesDemarches::Client.query(Queries::CreateDirectUpload, variables:
+      {
+        dossier_id: dossier_id,
+        filename: filename,
+        byteSize: File.size(path),
+        checksum: checksum,
+        contentType: (MIME::Types.type_for(path).presence || MIME::Types['text/plain']).first.to_s,
+        client_mutation_id: 'upload'
+      })
+    errors = result.errors&.values&.flatten.presence || result.data.to_h.values.first['errors']
+    throw errors.join(';') if errors.present?
+
+    result.data&.create_direct_upload
+  end
+
+  def self.checksum(file_path)
+    Digest::MD5.base64digest(File.read(file_path))
+  end
+
+  Queries = MesDemarches::Client.parse <<-'GRAPHQL'
+    mutation CreateDirectUpload($dossier_id: ID!, $filename: String!, $byteSize: Int!, $checksum: String!, $contentType: String!) {
+      createDirectUpload(input: {
+        dossierId: $dossier_id,
+        filename: $filename,
+        byteSize: $byteSize,
+        checksum: $checksum,
+        contentType: $contentType,
+        clientMutationId: "1"
+      }) {
+        clientMutationId,
+        directUpload {
+          headers,
+          signedBlobId,
+          url
+        }
+      }
+    }
+  GRAPHQL
+
+end
--- app/lib/set_annotation_value.rb
+++ app/lib/set_annotation_value.rb
@@ -17,16 +17,18 @@ class SetAnnotationValue
     annotation = get_annotation(md_dossier, annotation_name)
     if annotation.present?
       old_checksum = annotation.file&.checksum
-      new_checksum = checksum(path)
+      new_checksum = FileUpload.checksum(path)
       different_file = old_checksum != new_checksum
       if different_file
-        attachment = upload_file(md_dossier.id, new_checksum, path, filename)
+        attachment = FileUpload.upload_file(md_dossier.id, path, filename, new_checksum)
         raw_set_piece_justificative(md_dossier.id, instructeur_id, annotation.id, attachment)
       end
     else
       throw "Unable to find annotation '#{annotation_name}' on dossier #{md_dossier.number}"
     end
   end
+
+  private
 
   def self.raw_set_value(dossier_id, instructeur_id, annotation_id, value)
     result = MesDemarches::Client.query(typed_query(value), variables:
@@ -106,24 +108,6 @@ class SetAnnotationValue
       }
     }
 
-    mutation CreateDirectUpload($dossier_id: ID!, $filename: String!, $byteSize: Int!, $checksum: String!, $contentType: String!) {
-      createDirectUpload(input: {
-        dossierId: $dossier_id,
-        filename: $filename,
-        byteSize: $byteSize,
-        checksum: $checksum,
-        contentType: $contentType,
-        clientMutationId: "1"
-      }) {
-        clientMutationId,
-        directUpload {
-          headers,
-          signedBlobId,
-          url
-        }
-      }
-    }
-
     mutation SetPieceJustificative($dossier_id: ID!, $instructeur_id: ID!, $annotation_id: ID!, $attachment_id: ID!, $client_mutation_id: String!) {
       dossierModifierAnnotationPieceJustificative(input: {
         dossierId: $dossier_id
@@ -155,30 +139,6 @@ class SetAnnotationValue
     end
   end
 
-  def self.upload_file(dossier_id, checksum, path, filename)
-    slot = upload_slot(dossier_id, checksum, path, filename)
-    params = slot.direct_upload
-    response = Typhoeus.put(params.url, headers: JSON.parse(params.headers), body: File.read(path, mode: 'rb'))
-    throw response.response_body if response.code != 200
-    params.signed_blob_id
-  end
-
-  def self.upload_slot(dossier_id, checksum, path, filename)
-    result = MesDemarches::Client.query(Queries::CreateDirectUpload, variables:
-      {
-        dossier_id: dossier_id,
-        filename: filename,
-        byteSize: File.size(path),
-        checksum: checksum,
-        contentType: (MIME::Types.type_for(path).presence || MIME::Types['text/plain']).first.to_s,
-        client_mutation_id: 'upload'
-      })
-    errors = result.errors&.values&.flatten.presence || result.data.to_h.values.first['errors']
-    throw errors.join(';') if errors.present?
-
-    result.data&.create_direct_upload
-  end
-
   def self.raw_set_piece_justificative(dossier_id, instructeur_id, annotation_id, attachment_id)
     result = MesDemarches::Client.query(Queries::SetPieceJustificative, variables:
       {
@@ -193,7 +153,4 @@ class SetAnnotationValue
     result.data
   end
 
-  def self.checksum(file_path)
-    Digest::MD5.base64digest(File.read(file_path))
-  end
 end
