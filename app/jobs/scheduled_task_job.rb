132350df64c76e59822dc62440ede690e096463f
maatinito <15379878+maatinito@users.noreply.github.com>
2022-03-29 15:47:19 -1000

    ScheduledTask: remove debug mode

--- app/jobs/scheduled_task_job.rb
+++ app/jobs/scheduled_task_job.rb
@@ -1,23 +1,20 @@
 # frozen_string_literal: true
 
 class ScheduledTaskJob < CronJob
-  # self.schedule_expression = 'every day at noon'
+  # self.schedule_expression = 'every day at 7:00'
   self.schedule_expression = 'every 2 minute'
 
   def perform
-    # date_arel = ScheduledTask.arel_table[:run_at]
-    #   .where(date_arel.lteq(Date.today))
-    ScheduledTask.all.each do |scheduled|
+    date_arel = ScheduledTask.arel_table[:run_at]
+    ScheduledTask.where(date_arel.lteq(Date.today)).each do |scheduled|
       Rails.logger.tagged(scheduled.task) do
         task = InspectorTask.create_tasks([{ scheduled.task => JSON.parse(scheduled.parameters) }]).first
-        DossierActions.on_dossier(scheduled.dossier) do |dossier|
-          Rails.logger.tagged("#{dossier.demarche.number},#{dossier.number}") do
-            demarche = Demarche.find(dossier.demarche.number)
-            task.process(demarche, dossier)
-          end
-        end
+        throw "Impossible d'initialiser la tache #{scheduled.task}: #{task.errors.join(',')}" unless task.valid?
+
+        perform_task(scheduled, task)
         scheduled.destroy
       rescue StandardError => e
+        Sentry.capture_exception(e)
         Rails.logger.error("Error processing #{scheduled.task}: #{e.message}")
         e.backtrace.select { |b| b.include?('/app/') }.first(7).each { |b| Rails.logger.debug(b) }
       end
@@ -26,5 +23,16 @@ class ScheduledTaskJob < CronJob
 
   def max_attempts
     1
+  end
+
+  private
+
+  def perform_task(scheduled, task)
+    DossierActions.on_dossier(scheduled.dossier) do |dossier|
+      Rails.logger.tagged("#{dossier.demarche.number},#{dossier.number}") do
+        demarche = Demarche.find(dossier.demarche.number)
+        task.process(demarche, dossier)
+      end
+    end
   end
 end
