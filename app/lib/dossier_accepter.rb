9f0ccb5a073b846be33225cbf6b0195007227de8
Christian <15379878+maatinito@users.noreply.github.com>
2022-06-22 16:48:01 -1000

    Added dossier_accepter task

---
+++ app/lib/dossier_accepter.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+class DossierAccepter < DossierChangerEtat
+  attr_accessor :motivation
+
+  def initialize(params)
+    super
+    @motivation = @params[:motivation] || ''
+  end
+
+  def change_state(demarche, dossier)
+    result = MesDemarches::Client.query(Queries::Accepter, variables:
+      {
+        dossierId: dossier.id,
+        instructeurId: demarche.instructeur,
+        motivation:
+      })
+    throw StandardError.new result.errors if result.errors.present?
+  end
+
+  def authorized_fields
+    super + %i[motivation]
+  end
+
+  Queries = MesDemarches::Client.parse <<-'GRAPHQL'
+    mutation Accepter($dossierId: ID!, $instructeurId: ID!, $motivation: String!) {
+      dossierAccepter(input: {
+        dossierId: $dossierId,
+        instructeurId: $instructeurId,
+        motivation: $motivation,
+      }) {
+        clientMutationId
+        dossier {
+          id
+          number
+          state
+        }
+        errors
+      }
+    }
+  GRAPHQL
+end
