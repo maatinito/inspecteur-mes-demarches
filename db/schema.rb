b1708297124eaef64d1f29cacb615b5c9e80a67b
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2023-04-04 14:58:04 -1000

    rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,107 +12,105 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_23_014101) do
-
+ActiveRecord::Schema.define(version: 20_220_623_014_101) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "checks", force: :cascade do |t|
-    t.integer "dossier"
-    t.string "checker"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.datetime "checked_at"
-    t.bigint "version", default: 1
-    t.integer "demarche_id"
-    t.boolean "failed"
-    t.boolean "posted", default: false
-    t.index ["dossier", "checker"], name: "unicity", unique: true
-    t.index ["dossier"], name: "by_dossier"
+  create_table 'checks', force: :cascade do |t|
+    t.integer 'dossier'
+    t.string 'checker'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.datetime 'checked_at'
+    t.bigint 'version', default: 1
+    t.integer 'demarche_id'
+    t.boolean 'failed'
+    t.boolean 'posted', default: false
+    t.index %w[dossier checker], name: 'unicity', unique: true
+    t.index ['dossier'], name: 'by_dossier'
   end
 
-  create_table "delayed_jobs", force: :cascade do |t|
-    t.integer "priority", default: 0, null: false
-    t.integer "attempts", default: 0, null: false
-    t.text "handler", null: false
-    t.text "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string "locked_by"
-    t.string "queue"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string "cron"
-    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
+  create_table 'delayed_jobs', force: :cascade do |t|
+    t.integer 'priority', default: 0, null: false
+    t.integer 'attempts', default: 0, null: false
+    t.text 'handler', null: false
+    t.text 'last_error'
+    t.datetime 'run_at'
+    t.datetime 'locked_at'
+    t.datetime 'failed_at'
+    t.string 'locked_by'
+    t.string 'queue'
+    t.datetime 'created_at'
+    t.datetime 'updated_at'
+    t.string 'cron'
+    t.index %w[priority run_at], name: 'delayed_jobs_priority'
   end
 
-  create_table "demarches", force: :cascade do |t|
-    t.string "libelle"
-    t.datetime "checked_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "instructeur"
-    t.string "configuration"
+  create_table 'demarches', force: :cascade do |t|
+    t.string 'libelle'
+    t.datetime 'checked_at'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'instructeur'
+    t.string 'configuration'
   end
 
-  create_table "demarches_users", id: false, force: :cascade do |t|
-    t.bigint "demarche_id", null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id", "demarche_id"], name: "index_demarches_users_on_user_id_and_demarche_id"
+  create_table 'demarches_users', id: false, force: :cascade do |t|
+    t.bigint 'demarche_id', null: false
+    t.bigint 'user_id', null: false
+    t.index %w[user_id demarche_id], name: 'index_demarches_users_on_user_id_and_demarche_id'
   end
 
-  create_table "messages", force: :cascade do |t|
-    t.string "message"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "check_id"
-    t.string "field"
-    t.string "value"
-    t.index ["check_id"], name: "index_messages_on_check_id"
+  create_table 'messages', force: :cascade do |t|
+    t.string 'message'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.bigint 'check_id'
+    t.string 'field'
+    t.string 'value'
+    t.index ['check_id'], name: 'index_messages_on_check_id'
   end
 
-  create_table "scheduled_tasks", force: :cascade do |t|
-    t.integer "dossier"
-    t.string "task"
-    t.text "parameters"
-    t.datetime "run_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["dossier", "task", "run_at"], name: "st_unicity", unique: true
-    t.index ["run_at"], name: "by_date"
+  create_table 'scheduled_tasks', force: :cascade do |t|
+    t.integer 'dossier'
+    t.string 'task'
+    t.text 'parameters'
+    t.datetime 'run_at'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index %w[dossier task run_at], name: 'st_unicity', unique: true
+    t.index ['run_at'], name: 'by_date'
   end
 
-  create_table "syncs", force: :cascade do |t|
-    t.string "job"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'syncs', force: :cascade do |t|
+    t.string 'job'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.integer "failed_attempts", default: 0, null: false
-    t.string "unlock_token"
-    t.datetime "locked_at"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.integer 'sign_in_count', default: 0, null: false
+    t.datetime 'current_sign_in_at'
+    t.datetime 'last_sign_in_at'
+    t.string 'current_sign_in_ip'
+    t.string 'last_sign_in_ip'
+    t.string 'confirmation_token'
+    t.datetime 'confirmed_at'
+    t.datetime 'confirmation_sent_at'
+    t.string 'unconfirmed_email'
+    t.integer 'failed_attempts', default: 0, null: false
+    t.string 'unlock_token'
+    t.datetime 'locked_at'
+    t.index ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+    t.index ['unlock_token'], name: 'index_users_on_unlock_token', unique: true
   end
-
 end
