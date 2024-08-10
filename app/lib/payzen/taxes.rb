96d781dc490ac55602ebdb4e60f7f06049157011
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2024-08-09 14:48:43 -1000

    payzen/taxes: compute taxes only when amount is set

--- app/lib/payzen/taxes.rb
+++ app/lib/payzen/taxes.rb
@@ -5,7 +5,7 @@ module Payzen
     include ActionView::Helpers::NumberHelper
 
     def version
-      super + 1
+      super + 2
     end
 
     def required_fields
@@ -20,9 +20,12 @@ module Payzen
       super
       return unless must_check?(dossier)
 
-      montant = annotation(@params[:champ_montant_ht])&.value.to_i
-      tva = (montant * 0.13).round
-      ttc = montant + tva
+      amount = annotation(@params[:champ_montant_ht])&.value
+      return if amount.blank?
+
+      amount = amount.to_i
+      tva = (amount * 0.13).round
+      ttc = amount + tva
 
       different1 = SetAnnotationValue.set_value(@dossier, instructeur_id, @params[:champ_tva], tva) if @params[:champ_tva].present?
       different2 = SetAnnotationValue.set_value(@dossier, instructeur_id, @params[:champ_ttc], ttc) if @params[:champ_ttc].present?
