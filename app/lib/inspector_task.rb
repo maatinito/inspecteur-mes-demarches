b3a940fda95fbc54a4e63bcf2952bf4b69f73709
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2024-10-10 09:09:03 -1000

    inspector_task: better error message

--- app/lib/inspector_task.rb
+++ app/lib/inspector_task.rb
@@ -8,7 +8,7 @@ class InspectorTask
     @errors = []
     @params = params&.symbolize_keys || {}
     missing_fields = (required_fields - @params.keys)
-    @errors << "Les champs #{missing_fields.join(',')} devrait être définis sur #{self.class.name.underscore}" if missing_fields.present?
+    @errors << "Champ(s) manquant(s) '#{missing_fields.join(',')}' sur #{self.class.name.underscore}" if missing_fields.present?
     unknown_fields = @params.keys - authorized_fields - required_fields
     @errors << "#{unknown_fields.join(',')} n'existe(nt) pas sur #{self.class.name.underscore}" if unknown_fields.present?
     @messages = []
