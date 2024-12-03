7e8cda1a5909f178708cf36d46ea1d60599241d2
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2024-12-03 11:45:23 -1000

    stateless app: uses s3 bucket to store configuration files & publipost models.

---
+++ mirror_production.sh
@@ -0,0 +1,1 @@
+mc mirror --overwrite --remove --exclude '*.Identifier' mes-demarches-robot robot/mes-demarches-robot
---
+++ mirror_staging.sh
@@ -0,0 +1,1 @@
+mc mirror --overwrite --remove --exclude '*.Identifier' mes-demarches-robot-staging/ robot/mes-demarches-robot-staging
