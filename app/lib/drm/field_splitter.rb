c439d688c24fe91170f5005b2e501096060e168e
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2025-06-22 16:32:18 -1000

    drm/field_splitter: generic way to create attributes based on champ value split. Use mainly at DRM to split dropdown choice into multiple variables splitting input on '/'

---
+++ app/lib/drm/field_splitter.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module Drm
+  class FieldSplitter < FieldChecker
+    def version
+      super + 1
+    end
+
+    UNKNOWN = 'Unknown'
+    SEPARATOR_REGEX = %r{\s*/\s*}
+
+    def initialize(params)
+      super
+      @target_attributes = Array(@params[:attributs_cibles]).map(&:strip)
+    end
+
+    def required_fields
+      super + %i[champ_source attributs_cibles]
+    end
+
+    def process_row(_row, output)
+      source_field = field(params[:champ_source])
+      return unless source_field&.value
+
+      values = source_field.value.split(SEPARATOR_REGEX)
+
+      @target_attributes.each_with_index do |attribute_name, index|
+        output[attribute_name] = display(values[index])
+      end
+    end
+
+    private
+
+    def display(value)
+      value.presence || UNKNOWN
+    end
+  end
+end
